46ded58597d0838e94d7b3cad9732efb6ca61ff3
abid108 <abid1084@gmail.com>
2023-10-03 12:44:35 +0200

    5th

--- my_code/split_data.py
+++ my_code/split_data.py
@@ -12,7 +12,7 @@ def split_data(datapath, train_data_ratio, val_data_ratio, test_data_ratio):
     # Split with a ratio.
     # To only split into training and validation set, set a tuple to `ratio`, i.e, `(.8, .2)`.
     #Train, val, test
-    splitfolders.ratio(datapath, output="train_val_test", seed=42, ratio=(train_data_ratio, val_data_ratio, test_data_ratio), group_prefix=None) # default values
+    splitfolders.ratio(datapath, output="/home/abidhasan/Documents/Indicate_FH/train_val_test", seed=42, ratio=(train_data_ratio, val_data_ratio, test_data_ratio), group_prefix=None) # default values
 
 
     # Split val/test with a fixed number of items e.g. 100 for each set.
