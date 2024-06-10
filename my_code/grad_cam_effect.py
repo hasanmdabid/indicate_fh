350a930b5b036126b493799fd92ecba3e668db47
hasanmdabid <abid1084@gmail.com>
2024-06-10 10:03:03 +0200

    10_june_24

---
+++ my_code/grad_cam_effect.py
@@ -0,0 +1,153 @@
+# pylint: disable-all
+import logging
+import os
+import tensorflow as tf
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+tf.get_logger().setLevel(logging.ERROR)
+from IPython.display import Image, display
+from keras.utils import img_to_array, load_img
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib as mpl
+import random
+from skimage.transform import resize
+from IPython.display import Image as imgdisp, display
+from keras.models import load_model
+from clear_folder import clear_folder
+import keras
+
+# Define the paths to the folders and the weights file
+effected_folder = '/home/abidhasan/Documents/Indicate_FH/data/effected'
+not_effected_folder = '/home/abidhasan/Documents/Indicate_FH/data/not_effected'
+weights_path = '/home/abidhasan/Documents/Indicate_FH/saved_model/vgg16_300.h5'
+
+models = ['xception', 'mobilenetv2', 'vgg16', 'inceptionv3']
+
+n_channels = 3
+
+# Function to preprocess images
+img1 = "/home/abidhasan/Documents/Indicate_FH/train_val_test/test/effected/001_CLE204_Baseline_111.png"
+img2 = "/home/abidhasan/Documents/Indicate_FH/data/effected/001_CLE114_Baseline_112.png"
+img3 = "/home/abidhasan/Documents/Indicate_FH/data/effected/007_CLE203_Weizen_114.png"
+img4 = "/home/abidhasan/Documents/Indicate_FH/data/effected/CLE101_Baseline_Reaktion_CLE101_Baseline_Reaktion_3.png"
+img5 = "/home/abidhasan/Documents/Indicate_FH/data/effected/003_CLE170_Soja_78.png"
+
+destination_dir = '/home/abidhasan/Documents/Indicate_FH/grad_cam_images/'
+
+images = [img1, img2, img3, img4, img5]
+
+
+def get_img_array(img_path, size, image_nr):
+    # `img` is a PIL image of size sizexsize
+    img = keras.utils.load_img(img_path, target_size=size)
+    img.save(destination_dir+'original_'+f"{image_nr}"+'.png')
+    # `array` is a float32 Numpy array of shape (size, size, 3)
+    array = keras.utils.img_to_array(img)
+    # We add a dimension to transform our array into a "batch"
+    # of size (1, size, size, 3)
+    array = np.expand_dims(array, axis=0)
+    return array
+
+
+def make_gradcam_heatmap(img_array, model, last_conv_layer_name, pred_index=None):
+    # First, we create a model that maps the input image to the activations
+    # of the last conv layer as well as the output predictions
+    grad_model = tf.keras.models.Model(
+        [model.inputs], [model.get_layer(
+            last_conv_layer_name).output, model.output]
+    )
+
+    # Then, we compute the gradient of the top predicted class for our input image
+    # with respect to the activations of the last conv layer
+    with tf.GradientTape() as tape:
+        last_conv_layer_output, preds = grad_model(img_array)
+        if pred_index is None:
+            pred_index = tf.argmax(preds[0])
+        class_channel = preds[:, pred_index]
+
+    # This is the gradient of the output neuron (top predicted or chosen)
+    # with regard to the output feature map of the last conv layer
+    grads = tape.gradient(class_channel, last_conv_layer_output)
+
+    # This is a vector where each entry is the mean intensity of the gradient
+    # over a specific feature map channel
+    pooled_grads = tf.reduce_mean(grads, axis=(0, 1, 2))
+
+    # We multiply each channel in the feature map array
+    # by "how important this channel is" with regard to the top predicted class
+    # then sum all the channels to obtain the heatmap class activation
+    last_conv_layer_output = last_conv_layer_output[0]
+    heatmap = last_conv_layer_output @ pooled_grads[..., tf.newaxis]
+    heatmap = tf.squeeze(heatmap)
+
+    # For visualization purpose, we will also normalize the heatmap between 0 & 1
+    heatmap = tf.maximum(heatmap, 0) / tf.math.reduce_max(heatmap)
+    
+    return heatmap.numpy()
+
+
+for model_name in models:
+    print(model_name)
+    if model_name == 'xception':
+        size = 299
+        last_conv_layer_name = "block14_sepconv2_act"
+        
+    elif model_name == 'mobilenetv2':
+        size = 224
+        last_conv_layer_name = "block_16_depthwise"
+        
+    elif model_name == 'vgg16':
+        size = 224
+        last_conv_layer_name = 'block5_conv3'
+        
+    elif model_name == 'inceptionv3':
+        size = 299
+        last_conv_layer_name = 'mixed10'
+    
+    model = load_model('/home/abidhasan/Documents/Indicate_FH/saved_model/'+model_name+'_100.h5')
+
+
+    # Remove last layer's softmax
+    model.layers[-1].activation = None
+
+    
+    for img_idx, imagepath in enumerate(images):
+        img_array = get_img_array(imagepath, size=(size, size), image_nr=img_idx)
+        # Creat teh heatmap image
+        heatmap = make_gradcam_heatmap(img_array, model, last_conv_layer_name)
+        
+        def save_and_display_gradcam(imagepath, heatmap, cam_path='/home/abidhasan/Documents/Indicate_FH/grad_cam_images/'+model_name+'_'+ f"{img_idx}" +'.png', alpha=0.4):
+            # Load the original image
+            img = keras.utils.load_img(imagepath)
+            img = keras.utils.img_to_array(img)
+            print("Image shape {}".format(img.shape))
+            print("Image Max Pixel value: {}".format(np.max(img)))
+
+            # Rescale heatmap to a range 0-255
+            heatmap = np.uint8(255 * heatmap)
+
+            # Use jet colormap to colorize heatmap
+            jet = mpl.colormaps["jet"]
+
+            # Use RGB values of the colormap
+            jet_colors = jet(np.arange(256))[:, :3]
+            jet_heatmap = jet_colors[heatmap]
+
+            # Create an image with RGB colorized heatmap
+            jet_heatmap = keras.utils.array_to_img(jet_heatmap)
+            jet_heatmap = jet_heatmap.resize((img.shape[1], img.shape[0]))
+            jet_heatmap = keras.utils.img_to_array(jet_heatmap)
+
+            print('Shape of Jet_heatmap: {}'.format(jet_heatmap.shape))
+            print("Jet Heatmap Max Pixel value: {}".format(np.max(jet_heatmap)))
+
+            # Superimpose the heatmap on original image
+            superimposed_img = jet_heatmap * alpha + img
+            superimposed_img = keras.utils.array_to_img(superimposed_img)
+
+            # Save the superimposed image
+            superimposed_img.save(cam_path)
+
+        save_and_display_gradcam(imagepath, heatmap)
+
+
--- my_code/main.py
+++ my_code/main.py
@@ -1,24 +1,25 @@
-# pylint: disable-all
 
+# pylint: disable-all
 import logging
 import os
 import tensorflow as tf
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 tf.get_logger().setLevel(logging.ERROR)
-from sklearn.metrics import roc_curve, auc, confusion_matrix, ConfusionMatrixDisplay
-import matplotlib.pyplot as plt
-from keras import Model, layers
-from keras.models import load_model
-from keras.utils import Sequence
-import numpy as np
-import pandas as pd
-from keras.callbacks import ModelCheckpoint, EarlyStopping, ReduceLROnPlateau
-from split_data import *
-from clear_folder import clear_folder
 from sklearn.metrics import precision_recall_fscore_support, confusion_matrix
+from clear_folder import clear_folder
+from split_data import *
+from keras.callbacks import ModelCheckpoint, EarlyStopping, ReduceLROnPlateau
+import pandas as pd
+import numpy as np
+from keras.utils import Sequence
+from keras.models import load_model
+from keras import Model, layers
+import matplotlib.pyplot as plt
+from sklearn.metrics import roc_curve, auc, confusion_matrix, ConfusionMatrixDisplay
 
 
-models = ['xception', 'mobilenetv2', 'vgg16', 'inception']
+
+models = ['xception', 'mobilenetv2', 'vgg16', 'inceptionv3']
 # Initialize the data generator
 datapath = '/home/abidhasan/Documents/Indicate_FH/data'
 train_dir = '/home/abidhasan/Documents/Indicate_FH/train_val_test/train'
@@ -28,7 +29,6 @@ figpath = '/home/abidhasan/Documents/Indicate_FH/performance_figures'
 checkpointpath = '/home/abidhasan/Documents/Indicate_FH/checkpoints/'
 result_dir = '/home/abidhasan/Documents/Indicate_FH/results/'
 batch_size = 16
-dropout_rt = 0.4
 n_channels = 3
 epochs = 100
 
@@ -99,7 +99,7 @@ class DataGenerator(Sequence):
             combined = list(zip(self.image_files, self.labels))
             np.random.shuffle(combined)
             self.image_files, self.labels = zip(*combined)
-            
+
 
 clear_folder('/home/abidhasan/Documents/Indicate_FH/train_val_test')
 split_data(datapath, train_data_ratio, val_data_ratio, test_data_ratio)
@@ -131,52 +131,45 @@ for model_name in models:
     if model_name == 'xception':
         from keras.applications.xception import Xception
         size = 299
-        pretrained_model = Xception(weights='imagenet', input_shape=(
-            size, size, n_channels), include_top=False)
+        pretrained_model = Xception(weights='imagenet', input_shape=(size, size, n_channels), include_top=False)
     elif model_name == 'mobilenetv2':
-        size = 224
         from keras.applications.mobilenet_v2 import MobileNetV2
-        pretrained_model = MobileNetV2(
-            include_top=False,
-            input_shape=(size, size, n_channels),
-            weights='imagenet')
+        size = 224
+        pretrained_model = MobileNetV2(weights='imagenet', input_shape=(size, size, n_channels), include_top=False)
     elif model_name == 'vgg16':
         from keras.applications import VGG16
         size = 224
         # Load the pre-trained VGG16 model
-        vgg16_model = VGG16(weights='imagenet', include_top=False,
-                    input_shape=(size, size, n_channels))
-    elif model_name == 'inception':
+        pretrained_model = VGG16(weights='imagenet', input_shape=(size, size, n_channels), include_top=False)
+    elif model_name == 'inceptionv3':
         from keras.applications.inception_v3 import InceptionV3
         size = 299
-        pre_trained_model = InceptionV3(input_shape=(size, size, n_channels),  # Shape of our images
-                                include_top=False,  # Leave out the last fully connected layer
-                                weights='imagenet')
-    
+        pretrained_model = InceptionV3(weights='imagenet', input_shape=(size, size, n_channels), include_top=False, )
+
     for layer in pretrained_model.layers:
         layer.trainable = True
 
     x = pretrained_model.output
-    x = layers.GlobalAveragePooling2D()(x)
+    x = layers.Flatten()(x)
     x = layers.Dense(256, activation='relu')(x)
     x = layers.Dropout(0.2)(x)
     x = layers.Dense(64, activation='relu')(x)
     x = layers.Dropout(0.1)(x)
     predictions = layers.Dense(1,  activation='sigmoid')(x)
     model = Model(pretrained_model.input, predictions)
-    
+
     model.compile(loss='binary_crossentropy', optimizer=tf.keras.optimizers.Adam(
         learning_rate=0.0001), metrics=['accuracy'])
-    
+
     callbacks = [ModelCheckpoint(checkpointpath+model_name+'mdl_wts.hdf5', monitor='val_loss', mode='min', verbose=1, save_best_only=True),
                  ReduceLROnPlateau(monitor='val_loss', factor=0.3, patience=2, verbose=1, mode='min', min_lr=0.00000001)]
-    
+
     image_size = (size, size)
 
     train_gen = DataGenerator(image_dir=train_dir, batch_size=batch_size,
-                          image_size=image_size, n_channels=n_channels)
+                              image_size=image_size, n_channels=n_channels)
     test_gen = DataGenerator(image_dir=test_dir, batch_size=batch_size,
-                         image_size=image_size, n_channels=n_channels, shuffle=False)
+                             image_size=image_size, n_channels=n_channels, shuffle=False)
 
     # Train the model in batches
     steps_per_epoch = len(train_gen)
@@ -184,13 +177,13 @@ for model_name in models:
 
     print("[INFO] training head...")
     history = model.fit(train_gen, epochs=epochs, steps_per_epoch=steps_per_epoch,
-                    validation_data=test_gen, validation_steps=validation_steps, callbacks=callbacks)
-    
+                        validation_data=test_gen, validation_steps=validation_steps, callbacks=callbacks)
+
     model = load_model(checkpointpath+model_name+'mdl_wts.hdf5')
     model.save(model_dir+model_name+'_'+f"{epochs}"+'.h5')
-    
+
     model = load_model(model_dir+model_name+'_'+f"{epochs}"+'.h5')
-    
+
     # Plotting the Model performcaes, train Vs Validation accuracy and train vs Validation Losses.
     f, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 4))
     t = f.suptitle('f"{model_name}" Performance', fontsize=12)
@@ -206,7 +199,6 @@ for model_name in models:
     ax1.set_title('Accuracy')
     l1 = ax1.legend(loc="best")
 
-
     ax2.plot(epoch_list, history.history['loss'], label='Train Loss')
     ax2.plot(epoch_list, history.history['val_loss'], label='Validation Loss')
     ax2.set_xticks(np.arange(1, max_epoch, 5))
@@ -214,10 +206,10 @@ for model_name in models:
     ax2.set_xlabel('Epoch')
     ax2.set_title('Loss')
     l2 = ax2.legend(loc="best")
-    
+
     plt.savefig(figpath+'/'+model_name+'_TRAIN_VS_VALIDATION_LOSS_with_weights.eps',
                 format='eps', bbox_inches='tight')
-    
+
     # Evaluate the model on the test data
     loss, accuracy = model.evaluate(test_gen, steps=len(test_gen))
     print(f'Test accuracy: {accuracy * 100:.2f}%')
@@ -241,7 +233,7 @@ for model_name in models:
     # Plot the ROC curve
     plt.figure(figsize=(10, 5))
     plt.plot(fpr, tpr, color='darkorange', lw=2,
-            label=f'ROC curve (area = {roc_auc:.2f})')
+             label=f'ROC curve (area = {roc_auc:.2f})')
     plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
     plt.xlim([0.0, 1.0])
     plt.ylim([0.0, 1.05])
@@ -249,22 +241,21 @@ for model_name in models:
     plt.ylabel('True Positive Rate')
     plt.title('Receiver Operating Characteristic (ROC) Curve')
     plt.legend(loc='lower right')
-    
+
     plt.savefig(figpath+'/'+model_name+'_ROC.eps',
                 format='eps', bbox_inches='tight')
-    
+
     # Compute the confusion matrix
     cm = confusion_matrix(true_labels, predicted_classes)
 
     # Display the confusion matrix
     disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=[
-                                'Not Affected', 'Affected'])
+        'Not Affected', 'Affected'])
     disp.plot(cmap=plt.cm.Blues)
     plt.title('Confusion Matrix')
 
     plt.savefig(figpath+'/'+model_name+'_CM.eps',
                 format='eps', bbox_inches='tight')
-    
 
     # Calculate the confusion matrix
     conf_matrix = confusion_matrix(true_labels, predicted_classes)
@@ -300,8 +291,7 @@ for model_name in models:
     print(f"Macro Precision: {precision_macro}")
     print(f"Macro Recall: {recall_macro}")
     print(f"Macro F1 Score: {f1_macro}")
-    
-    
+
     # Data for plotting
     classes = np.unique(true_labels)
     x = np.arange(len(classes))
@@ -325,7 +315,7 @@ for model_name in models:
     # Save plot as EPS file
     plt.savefig(figpath+'/'+model_name+'_metrics_plot.eps',
                 format='eps', bbox_inches='tight')
-    
+
     # Save the results to a CSV file
     metrics_data = {
         'Class': list(range(len(precisions))) + ['Micro Average', 'Macro Average', 'Test Accuracy'],
@@ -336,8 +326,9 @@ for model_name in models:
     }
 
     metrics_df = pd.DataFrame(metrics_data)
-    #Save the results to the result directory
-    metrics_df.to_csv(result_dir+f'{model_name}_performance_metrics.csv', index=False)
+    # Save the results to the result directory
+    metrics_df.to_csv(
+        result_dir+f'{model_name}_performance_metrics.csv', index=False)
 
     print(
         f"Performance metrics for {model_name} saved to {model_name}_performance_metrics.csv")
